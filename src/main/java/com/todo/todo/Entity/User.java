4be9037f9e53228a18f13a4190b93b488d9f2eb8
Shriram kushwaha <117708615+shri214@users.noreply.github.com>
2024-09-16 23:25:02 +0530

    commit -m "my first api in java"

---
+++ src/main/java/com/todo/todo/Entity/User.java
@@ -0,0 +1,32 @@
+package com.todo.todo.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name="todoApp")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @Column(nullable = false, unique = true, length = 100, updatable = false)
+    private String Id;
+
+    @Column(nullable = false, length = 100)
+    private String userName;
+
+    @Column(nullable = false, unique = true, updatable = false)
+    private String email;
+
+    @Column(length = 255)
+    private String description;
+
+    @Column(nullable = false, updatable = true, length = 100)
+    private String statusAction;
+
+
+}
